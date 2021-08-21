9e2a0b165ec508490fa96cf8decd14b3720685ad
namthanh298 <69186819+namthanh298@users.noreply.github.com>
2021-08-21 18:53:52 +0700

    Update simple_ocr_with_tesseract.py

--- simple_ocr_with_tesseract.py
+++ simple_ocr_with_tesseract.py
@@ -10,19 +10,20 @@ import os
 import numpy as np
 import re
 import matplotlib.pyplot as plt
-pytesseract.pytesseract.tesseract_cmd = r'E:/Tesseract-OCR/tesseract.exe'    # trỏ đến đường dẫn của tesseract.exe
+pytesseract.pytesseract.tesseract_cmd = r'C:/.../tesseract.exe'           # trỏ đến đường dẫn của core file của thư viện Tesseract OCR: tesseract.exe
 
 
-# In[168]:
 
 
 # load the example image and convert it to grayscale
 image = cv2.imread('test.jpg')
-# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+""" Một số phép tiền xử lý ảnh trước khi đưa qua Tesseract nhận diện """
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # check to see if we should apply thresholding to preprocess the image
-#gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 # make a check to see if median blurring should be done to remove noise
-#gray = cv2.medianBlur(gray, 3)
+gray = cv2.medianBlur(gray, 3)
 # write the grayscale image to disk as a temporary file so we can apply OCR to it
 filename = "{}.png".format(os.getpid())
 cv2.imwrite(filename, image)
@@ -30,8 +31,6 @@ plt.imshow(image)
 
 
 # Cuối cùng chúng ta có thể áp dụng OCR cho hình ảnh của mình bằng cách sử dụng Tesseract Python:
-
-# In[169]:
 
 
 # load the image as a PIL/Pillow image, apply OCR, and then delete the temporary file
@@ -42,8 +41,6 @@ print(text)
 
 # # Thực hiện Text Localization, Text Detection và OCR với Tesseract
 
-# In[170]:
-
 
 # tải hình ảnh đầu vào, chuyển đổi từ kênh BGR sang RGB,
 # sử dụng Tesseract để localize từng vùng text trong hình ảnh đầu vào
@@ -51,8 +48,6 @@ rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 objects = pytesseract.image_to_data(rgb, lang='eng+vie', output_type=pytesseract.Output.DICT)
 objects.keys()          # In ra các key chính của objects
 
-
-# In[171]:
 
 
 # loop over each of the individual text localizations
@@ -73,13 +68,13 @@ for i in range(0, len(objects["text"])):
         print("Confidence: {}".format(conf))
         print("Text: {}".format(text))
         print("")
-        # strip out non-ASCII text so we can draw the text on the image
-        # using OpenCV, then draw a bounding box around the text along
-        # with the text itself
+        # tách các đoạn text hoặc kí tự không phải ASCII 
         text = "".join([c if ord(c) < 128 else "" for c in text]).strip()
+        # sử dụng OpenCV, sau đó vẽ một bounding box cho text
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
         #cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
         #            color=(0, 0, 255), fontScale=1, thickness=2)
+        
 cv2.imshow("Image with detected text", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
